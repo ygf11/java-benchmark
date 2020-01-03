7f2769585c7c9ea26274c11d5faddaf0c2d349c7
yanggaofeng <yanggf23@gmail.com>
2020-01-03 22:47:01 +0800

    add deserialialze test

---
+++ src/main/java/ygf/json/jmh/JsonDeserializeListTest.java
@@ -0,0 +1,12 @@
+package ygf.json.jmh;
+
+import org.openjdk.jmh.annotations.*;
+
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.SECONDS)
+@State(Scope.Benchmark)
+public class JsonDeserializeListTest {
+
+}
